f90d7f6681ebe1c874921be477da4037e2e4a809
Virginia Silva <silvavmo@stj.jus.br>
2020-12-10 16:34:50 -0300

    themes-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'themes/index'
+  get 'themes/edit'
+  get 'themes/new'
+  get 'themes/create'
+  get 'themes/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
