2da5a7e8a13fc0c765326580a4d3b0d67b93994d
Rafael Silva <rafaelvds@gmail.com>
2020-12-10 16:12:07 -0300

    added configure_permitted_parameters to application_controller; added validations to user model; added changes in views>devise>registrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :texts
+  has_many :themes
+
+  validates :name, :email, :password, :cpf, presence: true
+  validates :email, :cpf, uniqueness: true
+  has_one_attached :photo
 end
